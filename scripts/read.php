5cea1bbe0648971e0d0e02d624ef061d10f851f4
Tim Abrahamson <48040387+Tlabrahamson@users.noreply.github.com>
2019-12-12 16:29:59 -0600

    Update read.php

--- scripts/read.php
+++ scripts/read.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:dbname=registrationAppDb;host=localhost';
-    $user = 'registrationApp';
-    $password = 'getgood';
+    $user = '****';
+    $password = '****';
 
     try {
         $dbh = new PDO($dsn, $user, $password);
